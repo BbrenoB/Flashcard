a281046e5349a3132b9e61efdbad73a07ce5979a
BbrenoB <00001109925086sp@al.educacao.sp.gov.br>
2024-11-28 08:45:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'futebol',
+    'Quantos titulo mundiais o palmeiras tem?',
+    '0'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual time tem mais titulo mundiais?',
+    'São Paulo'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é a maior torcida do brasil?',
+    'Gaviôes da Fiel, Corinthians'
+)
+
+criaCartao(
+    'Futebol',
+    'Que ano foi a primeira copa do mundo?',
+    '1930'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual time do brasil com mais titulo?',
+    'Palmeiras'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem foi o rei do futebol?',
+    'Pelé'
+)
